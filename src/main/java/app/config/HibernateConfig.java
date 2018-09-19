d993fec5564579edf6065832653dd716f612c2ac
svinogr <svinogr@hotmail.com>
2018-09-19 19:39:14 +0300

    add dialect mysql for mysql 5.5

--- src/main/java/app/config/HibernateConfig.java
+++ src/main/java/app/config/HibernateConfig.java
@@ -29,7 +29,7 @@ public class HibernateConfig {
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("app.*");
+        sessionFactory.setPackagesToScan("app");
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
